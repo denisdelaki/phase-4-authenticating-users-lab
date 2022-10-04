15d520de2fe366230142f3af2759b00369301762
denisdelaki <denisdelaki@gmail.com>
2022-10-04 09:46:20 +0300

    commit tree b42e4eabc4231782c4816a1f8b8a05492c746aaf

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+def create 
+    #find the user in the database
+user=User.find_by(username:params[:username])
+#save the user id to the session hash 
+session[:user]=user.id
+render json: user
+end
+def destroy 
+session.delete :user_id 
+head :no_content
+end
+end
